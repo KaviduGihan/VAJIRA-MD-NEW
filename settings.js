3215cb4d07954bcd8b4cec565e3e8899c0cfffd8
KaviduGihan <119643040+KaviduGihan@users.noreply.github.com>
2024-09-11 22:02:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=GBl0TZLK#NCaerZL3zmV1R-L_N7InQ_kOXw-atXXZjA6uQcs66JQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=vFkw0LDS#eY71fqpecadszi1MIrU4ep6QJfmio3EiL0UkLE-G_gk' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+94768745948' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
